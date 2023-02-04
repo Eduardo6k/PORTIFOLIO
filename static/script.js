46b22cab5333d63ad44b07ba8dcebfba79105331
EDUARDO ALVES MARTINS NETO <108354728+Eduardo6k@users.noreply.github.com>
2023-02-04 16:00:17 -0300

    habilidades e curriculo

--- static/script.js
+++ static/script.js
@@ -8,3 +8,15 @@ menuMobile.addEventListener('click',()=>{
     : menuMobile.classList.replace('bi-x','bi-list');
     body.classList.toggle('menu-nav-active')
 });
+// fecha o menu quando clicar em algum item e mudar o icone para list
+
+const navItem = document.querySelectorAll('.nav-item')
+
+navItem.forEach(item=>{
+    item.addEventListener("click", ()=>{
+        if(body.classList.contains("menu-nav-active")){
+            body.classList.remove("menu-nav-active")
+            menuMobile.classList.replace("bi-x","bi-list");
+        }
+    })
+})
